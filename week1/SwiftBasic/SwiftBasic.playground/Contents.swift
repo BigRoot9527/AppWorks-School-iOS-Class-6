99f74ed0d5751cf11d4d6e1a6fd5bf24d7a93a7d
許庭瑋 <jeremiah79324@gmail.com>
2018-02-11 17:44:30 +0800

    finish week1-Swift Basic

---
+++ week1/SwiftBasic/SwiftBasic.playground/Contents.swift
@@ -0,0 +1,46 @@
+//Week1 - Swift Basic
+
+//1.
+let PI = 3.14
+
+//2.
+let x: Int = 66
+let y: Int = 87
+let average = (x + y) / 2
+
+//3.
+//change the types of constant x and y into Double
+10/3
+10/3.0
+/* 10/3 can be converted to specified type Int(= 3) or Double(= 3.333),where 10/3.0 represend an Double type number that can only be converted to specified type Double */
+
+//4.
+var flag: Bool = true
+var inputString: String = "Hello world."
+let bitsInBite: Int = 8
+let averageScore: Double = 86.8
+
+//5.
+var salary = 22000
+salary += 28000
+
+//6.
+// 0 is not false
+//since "var check: Bool = 0" will causes a crash because of the type difference
+
+//7.
+let firValue = 10
+let secValue = 3
+let remain = firValue%secValue
+
+
+
+
+
+
+
+
+
+
+
+
